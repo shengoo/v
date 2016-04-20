c4d21aef6444697ed62ec910c2e0338951a2f6e4
shengoo <shengoo@hotmail.com>
2016-04-20 23:52:26 +0800

    add views

--- client/android/app/src/main/java/com/sheng00/vis/MainActivity.java
+++ client/android/app/src/main/java/com/sheng00/vis/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewAnimator;
+import android.widget.ViewSwitcher;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
@@ -11,19 +15,37 @@ import com.roughike.bottombar.OnMenuTabClickListener;
 public class MainActivity extends AppCompatActivity {
 
     private BottomBar mBottomBar;
+    private ViewSwitcher container;
+
+    Animation slide_in_left, slide_out_right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        container = (ViewSwitcher)findViewById(R.id.container);
+
+        slide_in_left = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_in_left);
+        slide_out_right = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_out_right);
+
+//        container.setInAnimation(slide_in_left);
+//        container.setOutAnimation(slide_out_right);
+
         mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.setActiveTabColor("#009688");
         mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
             @Override
             public void onMenuTabSelected(@IdRes int menuItemId) {
-                if (menuItemId == R.id.bottomBarItemOne) {
-                    // The user selected item number one.
+                switch (menuItemId){
+                    case R.id.bottomBarItemOne:
+                        container.setDisplayedChild(0);
+                        break;
+                    case R.id.bottomBarItemTwo:
+                        container.setDisplayedChild(1);
+                        break;
                 }
             }
 
@@ -31,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             public void onMenuTabReSelected(@IdRes int menuItemId) {
                 if (menuItemId == R.id.bottomBarItemOne) {
                     // The user reselected item number one, scroll your content to top.
+//                    container.setDisplayedChild(1);
                 }
             }
         });
