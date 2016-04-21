f3921187f6db8cd69eb708cd6e965d2abfc36453
shengoo <shengoo@hotmail.com>
2016-04-21 22:05:25 +0800

    fragment

--- client/android/app/src/main/java/com/sheng00/vis/fragments/BlankFragment.java
+++ client/android/app/src/main/java/com/sheng00/vis/fragments/BlankFragment.java
@@ -10,14 +10,7 @@ import android.view.ViewGroup;
 
 import com.sheng00.vis.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BlankFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BlankFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class BlankFragment extends Fragment {
 
 
